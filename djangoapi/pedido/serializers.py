7b0cdb08063d9708161760402002051cf92c5613
Igor Paim <igorp984@gmail.com>
2019-03-08 03:31:55 -0300

    criado a opção atualização do pedido

--- djangoapi/pedido/serializers.py
+++ djangoapi/pedido/serializers.py
@@ -4,7 +4,7 @@ from .models import Item, Pedido
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ('produto', 'quantidade', 'preco_unitario')
+        fields = ('pk','produto', 'quantidade', 'preco_unitario')
 
 class PedidoSerializer(serializers.ModelSerializer):
     item_set = ItemSerializer(many=True)
@@ -20,9 +20,15 @@ class PedidoSerializer(serializers.ModelSerializer):
             Item.objects.create(pedido=pedido, **item_data)
         return pedido
 
-    def update(self, isinstance, validated_data):
+    def update(self, instance, validated_data):
         itens_data = validated_data.pop('item_set')
-        pedido = Pedido.objects.create(**validated_data)
+        instance.client = validated_data.get('client', instance.client)
+        itens = list(instance.item_set.all())
+        instance.save()
         for item_data in itens_data:
-            Item.objects.create(pedido=pedido, **item_data)
-        return pedido
+            item = itens.pop(0)
+            item.produto = item_data.get('produto', item.produto)
+            item.quantidade = item_data.get('quantidade', item.quantidade)
+            item.preco_unitario = item_data.get('preco_unitario', item.preco_unitario)
+            item.save()
+        return instance
